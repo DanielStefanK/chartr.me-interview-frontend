ee9e271a6751d6c222a9b0822961ba82fb199d18
Daniel Stefan Klose <daniel-stefan.klose@udo.edu>
2019-01-18 22:58:52 +0100

    added redirect for page not found

--- src/router.js
+++ src/router.js
@@ -7,9 +7,16 @@ import EntryPoint from '@/components/EntryPoint';
 export default new Router({
   routes: [
     {
-      path: '/:id',
+      path: '/interview/:id',
       name: 'home',
       component: EntryPoint,
+    },
+    {
+      path: '*',
+      redirect: () => {
+        window.location.href =
+          process.env.VUE_APP_LANDINGPAGE || 'https://chartr.me/';
+      },
     },
   ],
 });
